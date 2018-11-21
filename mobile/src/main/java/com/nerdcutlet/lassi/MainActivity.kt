0a7fc0a5e7fd783de4e8f39277683b92af2a7082
Aldrich <mascarenhasaldrich@gmail.com>
2018-11-21 15:29:54 +0000

    Initial Commit

---
+++ mobile/src/main/java/com/nerdcutlet/lassi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nerdcutlet.lassi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
